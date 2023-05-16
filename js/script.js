5c840af3dcfcdcae72006fa19003f42f1280fe50
Thiago Souza <thiago.smprado@gmail.com>
2023-05-16 20:57:53 -0300

    Up

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ const horas = document.getElementById('horas');
 const minutos = document.getElementById('minutos');
 const segundos = document.getElementById('segundos');
 const tema = document.getElementById('btn-aparencia');
+const backhora = document.getElementsByClassName('fundoHora');
+const backgroundBody = document.querySelector("body");
 
 const relogio = setInterval(function time () {
   let dateToday = new Date();
@@ -19,8 +21,15 @@ const relogio = setInterval(function time () {
 })
 
 tema.addEventListener("click", function(){
+  console.log({backhora});
   this.style.background = "white";
-  var backgroundBody = document.querySelector("body");
-  backgroundBody.style.background = "black";
-  this.innerText = "Tema: Escuro";
+  backgroundBody.style.background = backgroundBody.style.background === "black" ? "white" : "black";
+  backhora[0].style.background = backgroundBody.style.background === "black" ? "white" : "black";
+  backhora[1].style.background = backgroundBody.style.background === "black" ? "white" : "black";
+  backhora[2].style.background = backgroundBody.style.background === "black" ? "white" : "black";
+  backhora[0].style.color = backgroundBody.style.background === "black" ? "black" : "white";
+  backhora[1].style.color = backgroundBody.style.background === "black" ? "black" : "white";
+  backhora[2].style.color = backgroundBody.style.background === "black" ? "black" : "white";
+  this.innerText = `Tema: ${backgroundBody.style.background === "black" ? "Escuro" : "Claro"}`;
+  tema.style.backgroundColor = backgroundBody.style.background === "black" ? "white" : "#8ccece";
 })
